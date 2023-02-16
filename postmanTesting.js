5a56cee47c0d7cc2c5866b68a074a150b8d3fdf3
montymi <mcmontanaro01@gmail.com>
2023-02-16 08:16:19 -0500

    Adding the Postman Testing script and the response that the schema is compared to

---
+++ postmanTesting.js
@@ -0,0 +1,32 @@
+const failedSchema = 'https://raw.githubusercontent.com/montymi/SchemaAPITesting/main/failed-schema.json';
+const passedSchema = 'https://raw.githubusercontent.com/montymi/SchemaAPITesting/main/passed-schema.json'
+
+function getRequest(schemaUrl) {
+  return new Promise(function(resolve, reject) {
+    pm.sendRequest({
+      url: schemaUrl,
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json',
+      }
+    }, function(err, res) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res.json());
+      }
+    });
+  });
+}
+
+getRequest(failedSchema)
+  .then(function(response) {
+    console.log(response);
+    // Do something with the response here
+    pm.test("Validate Schema", () => {
+        pm.response.to.have.jsonSchema(response)
+    });
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
